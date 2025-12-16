84f39ac6ff244b166347cd77810d70f852fea206
Jake Boateng <64sjjsi@students.codeinstitute.net>
2025-12-16 13:26:09 +0000

    refactor(admin): improve dashboard layout and responsiveness

--- Admin/src/pages/Home.jsx
+++ Admin/src/pages/Home.jsx
@@ -1,4 +1,3 @@
-// Admin/src/pages/Home.jsx
 import { useEffect, useMemo, useRef, useState } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 import { HiArrowTrendingUp, HiArrowTrendingDown } from "react-icons/hi2";
